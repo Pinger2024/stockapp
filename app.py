caca0fa1aec8f9c97f7de35aeb5cdfea88fb49aa
Michael James <michaeljames@Michaels-iMac.local>
2024-10-01 12:40:19 +0100

    Add ticker count to the webpage

--- app.py
+++ app.py
@@ -13,8 +13,11 @@ def index():
     # Query the MongoDB collection for the first 5 documents
     stock_data = list(collection.find().limit(5))
 
+    # Count the number of distinct tickers in the collection
+    unique_tickers_count = len(collection.distinct('ticker'))
+
     # Pass data to the HTML template to render on the page
-    return render_template('index.html', stocks=stock_data)
+    return render_template('index.html', stocks=stock_data, tickers_count=unique_tickers_count)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
